545c54ed5d2427cc09dc1d59e354c83526d62305
Matias Benary <matiasbenary@gmail.com>
2021-06-25 18:13:54 -0300

    Logica de comer a refactorizar

--- app.js
+++ app.js
@@ -8,91 +8,174 @@ const matrix = [];
 
 let previousClickCell = null;
 
-const clickCell =(e) =>{
+const clickCell = (e) => {
   const nextClickCell = e.target;
 
-  if(previousClickCell === nextClickCell){
-    console.log("same");
-    previousClickCell.classList.remove("cell--selected");
-    previousClickCell = null;
-  }else if(previousClickCell){
-    console.log("new");
+  if (previousClickCell === nextClickCell) {
+    blurCell();
+  } else if (previousClickCell) {
+    const columnPrevious = previousClickCell.dataset.column;
+    const columnNext = nextClickCell.dataset.column;
+    const rowPrevius = previousClickCell.dataset.row;
+    const rowNext = nextClickCell.dataset.row;
 
-    const diffX = previousClickCell.dataset.row - nextClickCell.dataset.row;
-    const diffY = previousClickCell.dataset.column - nextClickCell.dataset.column;
-    console.log({diffX,diffY},(diffX >= -1 && diffX <=1),(diffY >= -1 && diffY <=1));
-    if((diffX >= -1 && diffX <=1) && (diffY >= -1 && diffY <=1)){
-      nextClickCell.classList.add("cell--selected");
-
-      const columnPrevious = previousClickCell.dataset.column;
-      const columnNext = nextClickCell.dataset.column;
-      const rowPrevius = previousClickCell.dataset.row;
-      const rowNext = nextClickCell.dataset.row;
-
-      nextClickCell.style.top = `${ columnPrevious * cell_size}px`
-      nextClickCell.style.left = `${ rowPrevius * cell_size}px`
-      previousClickCell.style.top = `${ columnNext * cell_size}px`
-      previousClickCell.style.left = `${ rowNext * cell_size}px`
-
-      nextClickCell.dataset.row = rowPrevius
-      nextClickCell.dataset.column = columnPrevious
-
-      previousClickCell.dataset.row = rowNext
-      previousClickCell.dataset.column = columnNext
-      previousClickCell.classList.remove("cell--selected");
-      nextClickCell.classList.remove("cell--selected");
-
-      previousClickCell = null;
+    if (!isAdjacent(rowPrevius, rowNext, columnPrevious, columnNext)) {
+      return;
     }
-  }else{
-    console.log("old");
-    previousClickCell = nextClickCell;
-    previousClickCell.classList.add("cell--selected");
+
+    nextClickCell.classList.add('cell--selected');
+
+    moveToCell(nextClickCell, columnPrevious, rowPrevius);
+    moveToCell(previousClickCell, columnNext, rowNext);
+
+    const rowMatcing = getRowMatching();
+    const columnMatcing = getColumnMatching();
+    console.log(rowMatcing);
+    console.log(columnMatcing);
+
+    if (rowMatcing.length || columnMatcing.length) {
+      console.log('eat');
+    } else {
+      setTimeout(
+        (
+          columnPrevious,
+          rowPrevius,
+          columnNext,
+          rowNext,
+          previousClickCell,
+          nextClickCell
+        ) => {
+          moveToCell(previousClickCell, columnPrevious, rowPrevius);
+          moveToCell(nextClickCell, columnNext, rowNext);
+        },
+        500,
+        columnPrevious,
+        rowPrevius,
+        columnNext,
+        rowNext,
+        previousClickCell,
+        nextClickCell
+      );
+    }
+
+    previousClickCell.classList.remove('cell--selected');
+    nextClickCell.classList.remove('cell--selected');
+    previousClickCell = null;
+  } else {
+    focusCell(nextClickCell);
   }
-}
+};
 
+const blurCell = () => {
+  previousClickCell.classList.remove('cell--selected');
+  previousClickCell = null;
+};
 
+const focusCell = (nextClickCell) => {
+  previousClickCell = nextClickCell;
+  previousClickCell.classList.add('cell--selected');
+};
+
+const isAdjacent = (rowPrevius, rowNext, columnPrevious, columnNext) => {
+  const diffX = rowPrevius - rowNext;
+  const diffY = columnPrevious - columnNext;
+
+  return diffX >= -1 && diffX <= 1 && diffY >= -1 && diffY <= 1;
+};
+
+const moveToCell = (cell, column, row) => {
+  
+  cell.style.top = `${column * cell_size}px`;
+  cell.style.left = `${row * cell_size}px`;
+  cell.dataset.row = row;
+  cell.dataset.column = column;
+  matrix[column][row] = cell.innerHTML;
+};
 const getRandom = () => {
   return Math.round(Math.random() * (EMOJIS.length - 1));
 };
 
 const generateMatrix = () => {
+  //0,1,2,3,4,5,6,7,8
   for (let column = 0; column < matrix_size; column++) {
     matrix[column] = [];
+    //0,1,2,3,4,5,6,7,8
     for (let row = 0; row < matrix_size; row++) {
       const indexEmoji = getRandom();
       const emoji = EMOJIS[indexEmoji];
 
-      matrix[column][row] = indexEmoji;
-
+      matrix[column][row] = emoji;
       matrixDom.append(generateCells(column, row, emoji));
-
     }
   }
 };
 
-const generateCells = (column,row,emoji) => {
-  const square = document.createElement('div')
+const generateCells = (column, row, emoji) => {
+  const square = document.createElement('div');
+  square.classList.add('cell');
+  square.innerHTML = emoji;
 
-  square.classList.add('cell')
-  square.dataset.row = row
-  square.dataset.column = column
-  square.id=`${row}-${column}`
+  square.addEventListener('click', clickCell);
 
-  square.innerHTML = emoji
+  square.style.top = `${column * cell_size}px`;
+  square.style.left = `${row * cell_size}px`;
+  square.style.width = `${cell_size - 2}px`;
+  square.style.height = `${cell_size - 2}px`;
 
-  square.addEventListener('click',clickCell)
-
-  square.style.top = `${column * cell_size}px`
-  square.style.left = `${row * cell_size}px`
-  square.style.width = `${cell_size  -2}px`
-  square.style.height = `${cell_size  -2}px`
+  square.dataset.row = row;
+  square.dataset.column = column;
+  // square.id=`${row}-${column}`
 
   return square;
-}
+};
 
-generateMatrix();
+const getRowMatching = () => {
+  const coords=[];
+  for (let column = 0; column < matrix_size; column++) {
+    let previous = null;
+    let cont;
+    for (let row = 0; row < matrix_size; row++) {
+      if (previous != matrix[column][row]) {
+        previous = matrix[column][row];
+        cont = 0;
+      } else {
+        cont++;
+      }
+      if (cont >= 2) {
+        let subRow = row;
+        while(subRow < matrix_size && previous == matrix[column][subRow]){
+          subRow++;
+        }
+        coords.push([[column, row-2],[column,subRow-1]]);
+      }
+    }
+  }
+  return coords;
+};
 
+const getColumnMatching = () => {
+  const coords=[];
+  for (let row = 0; row < matrix_size; row++) {
+    let previous = null;
+    let cont;
+    for (let column = 0; column < matrix_size; column++) {
+      if (previous != matrix[column][row]) {
+        previous = matrix[column][row];
+        cont = 0;
+      } else {
+        cont++;
+      }
+      if (cont >= 2) {
+        const init = column-2;
+        while(column < matrix_size && previous == matrix[column][row]){
+          column++;
+        }
+        coords.push([[init, row],[column-1,row]]);
+      }
+    }
+  }
+  return coords;
+};
 
 // const drawMatrix = () =>{
 //   for (let i = 0; i < grilla.length; i++) {
@@ -105,3 +188,8 @@ generateMatrix();
 //     }
 //   }
 // }
+
+do {
+  matrixDom.innerHTML = '';
+  generateMatrix();
+} while (getRowMatching().length || getColumnMatching().length);
