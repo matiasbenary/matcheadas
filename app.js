872e1db8a181faf7fd42bf40246e019686e3c8a8
Matias Benary <matiasbenary@gmail.com>
2021-06-26 16:18:45 -0300

    :pencil: se agrego borrar elementos y bajar elementos

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const cell_size = 56;
 const matrix = [];
 
 let previousClickCell = null;
-
+let nextClickCell = null;
 const clickCell = (e) => {
-  const nextClickCell = e.target;
+  nextClickCell = e.target;
 
   if (previousClickCell === nextClickCell) {
     blurCell();
@@ -20,13 +20,14 @@ const clickCell = (e) => {
     const rowNext = nextClickCell.dataset.row;
 
     if (!isAdjacent(rowPrevius, rowNext, columnPrevious, columnNext)) {
+      blurCell();
+      focusCell();
       return;
     }
 
     nextClickCell.classList.add('cell--selected');
 
-    moveToCell(nextClickCell, columnPrevious, rowPrevius);
-    moveToCell(previousClickCell, columnNext, rowNext);
+    switchCell(columnPrevious, columnNext, rowPrevius, rowNext);
 
     const rowMatcing = getRowMatching();
     const columnMatcing = getColumnMatching();
@@ -34,36 +35,97 @@ const clickCell = (e) => {
     console.log(columnMatcing);
 
     if (rowMatcing.length || columnMatcing.length) {
-      console.log('eat');
+      previousClickCell.classList.remove('cell--selected');
+      nextClickCell.classList.remove('cell--selected');
+      processMatch(rowMatcing, columnMatcing);
+      dropCell(rowMatcing, columnMatcing);
+
+      previousClickCell = null;
     } else {
       setTimeout(
-        (
-          columnPrevious,
-          rowPrevius,
-          columnNext,
-          rowNext,
-          previousClickCell,
-          nextClickCell
-        ) => {
-          moveToCell(previousClickCell, columnPrevious, rowPrevius);
-          moveToCell(nextClickCell, columnNext, rowNext);
+        (columnPrevious, rowPrevius, columnNext, rowNext) => {
+          switchCell(columnNext, columnPrevious, rowNext, rowPrevius);
+          previousClickCell.classList.remove('cell--selected');
+          nextClickCell.classList.remove('cell--selected');
+          previousClickCell = null;
         },
         500,
         columnPrevious,
         rowPrevius,
         columnNext,
-        rowNext,
-        previousClickCell,
-        nextClickCell
+        rowNext
       );
     }
-
-    previousClickCell.classList.remove('cell--selected');
-    nextClickCell.classList.remove('cell--selected');
-    previousClickCell = null;
   } else {
     focusCell(nextClickCell);
   }
+};
+
+const processMatch = (rowMatcing, columnMatcing) => {
+  for (const matching of rowMatcing) {
+    const column = matching[0][0];
+    for (let row = matching[0][1]; row <= matching[1][1]; row++) {
+      matrix[column][row] = null;
+      getCellDom(column, row).innerHTML = '';
+    }
+  }
+
+  for (const matching of columnMatcing) {
+    const row = matching[0][1];
+    for (let column = matching[0][0]; column <= matching[1][0]; column++) {
+      matrix[column][row] = null;
+      getCellDom(column, row).innerHTML = '';
+    }
+  }
+};
+
+const dropCell = (rowMatcing, columnMatcing) => {
+  for (const matching of columnMatcing) {
+    const row = matching[0][1];
+    const numberOfItems = matching[1][0] - matching[0][0] + 1;
+    lowerColumn(row, matching[1][0], numberOfItems);
+  }
+
+  for (const matching of rowMatcing) {
+    const column = matching[0][0];
+    for (let row = matching[0][1]; row <= matching[1][1]; row++) {
+      lowerColumn(row, column, 1);
+    }
+  }
+
+  rowMatcing = getRowMatching();
+  columnMatcing = getColumnMatching();
+
+  if (rowMatcing.length || columnMatcing.length) {
+    dropCell(rowMatcing, columnMatcing);
+  }
+};
+
+const lowerColumn = (row, finished, numberOfItems) => {
+  for (let column = finished; column >= 0; column--) {
+    if (column - numberOfItems >= 0) {
+      matrix[column][row] = matrix[column - numberOfItems][row];
+      getCellDom(column, row).innerHTML = matrix[column - numberOfItems][row];
+    } else {
+      const indexEmoji = getRandom();
+      const emoji = EMOJIS[indexEmoji];
+      matrix[column][row] = emoji;
+      getCellDom(column, row).innerHTML = emoji;
+    }
+  }
+};
+
+const switchCell = (columnPrevious, columnNext, rowPrevius, rowNext) => {
+  const nextEmoji = matrix[columnNext][rowNext];
+  const previusEmoji = matrix[columnPrevious][rowPrevius];
+  moveToCell(nextClickCell, columnPrevious, rowPrevius, nextEmoji);
+  moveToCell(previousClickCell, columnNext, rowNext, previusEmoji);
+};
+
+const getCellDom = (column, row) => {
+  return document.querySelector(
+    `.cell[data-column="${column}"][data-row="${row}"]`
+  );
 };
 
 const blurCell = () => {
@@ -71,7 +133,7 @@ const blurCell = () => {
   previousClickCell = null;
 };
 
-const focusCell = (nextClickCell) => {
+const focusCell = () => {
   previousClickCell = nextClickCell;
   previousClickCell.classList.add('cell--selected');
 };
@@ -83,13 +145,12 @@ const isAdjacent = (rowPrevius, rowNext, columnPrevious, columnNext) => {
   return diffX >= -1 && diffX <= 1 && diffY >= -1 && diffY <= 1;
 };
 
-const moveToCell = (cell, column, row) => {
-  
+const moveToCell = (cell, column, row, emoji) => {
   cell.style.top = `${column * cell_size}px`;
   cell.style.left = `${row * cell_size}px`;
   cell.dataset.row = row;
   cell.dataset.column = column;
-  matrix[column][row] = cell.innerHTML;
+  matrix[column][row] = emoji;
 };
 const getRandom = () => {
   return Math.round(Math.random() * (EMOJIS.length - 1));
@@ -130,7 +191,7 @@ const generateCells = (column, row, emoji) => {
 };
 
 const getRowMatching = () => {
-  const coords=[];
+  const coords = [];
   for (let column = 0; column < matrix_size; column++) {
     let previous = null;
     let cont;
@@ -143,10 +204,14 @@ const getRowMatching = () => {
       }
       if (cont >= 2) {
         let subRow = row;
-        while(subRow < matrix_size && previous == matrix[column][subRow]){
+        while (subRow < matrix_size && previous == matrix[column][subRow]) {
           subRow++;
         }
-        coords.push([[column, row-2],[column,subRow-1]]);
+        coords.push([
+          [column, row - 2],
+          [column, subRow - 1],
+        ]);
+        cont = 0;
       }
     }
   }
@@ -154,7 +219,7 @@ const getRowMatching = () => {
 };
 
 const getColumnMatching = () => {
-  const coords=[];
+  const coords = [];
   for (let row = 0; row < matrix_size; row++) {
     let previous = null;
     let cont;
@@ -166,11 +231,15 @@ const getColumnMatching = () => {
         cont++;
       }
       if (cont >= 2) {
-        const init = column-2;
-        while(column < matrix_size && previous == matrix[column][row]){
+        const init = column - 2;
+        while (column < matrix_size && previous == matrix[column][row]) {
           column++;
         }
-        coords.push([[init, row],[column-1,row]]);
+        coords.push([
+          [init, row],
+          [column - 1, row],
+        ]);
+        cont = 0;
       }
     }
   }
@@ -188,7 +257,6 @@ const getColumnMatching = () => {
 //     }
 //   }
 // }
-
 do {
   matrixDom.innerHTML = '';
   generateMatrix();
